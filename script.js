bb8245a53e8eb51ec2dc2fc5de300648df69415c
Melissa Utomo <mafutomo@gmail.com>
2017-10-11 16:07:14 -0600

    updated script file to add checkerboard DOM capability

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
-// Your JS goes here
+// Each tile should be a div
+// Each tile's width is 11.1%
+// Set each tile's float property to left
+// Each tile's paddingBottom is 11.1%// Your JS goes here
+
+// STARTING WITH RED
+
+for (let i = 0; i < 5; i++ ) {
+
+for (let i = 0; i < 9; i++) {
+
+var div = document.createElement('div');
+var body = document.getElementsByTagName('body')[0];
+
+div.style.float = 'left';
+div.style.paddingBottom = '11.1%';
+div.style.width = '11.1%';
+body.prepend(div);
+if (i % 2 === 0) {
+  div.style.backgroundColor = 'black';
+} else {
+  div.style.backgroundColor = 'red';
+}
+
+}
+
+// STARTING WITH BLACK
+for (let i = 0; i < 9; i++) {
+
+var div = document.createElement('div');
+var body = document.getElementsByTagName('body')[0];
+
+div.style.float = 'left';
+div.style.paddingBottom = '11.1%';
+// div.style.backgroundColor = 'red';
+div.style.width = '11.1%';
+body.prepend(div);
+
+if (i % 2 === 0) {
+  div.style.backgroundColor = 'red';
+} else {
+  div.style.backgroundColor = 'black';
+}
+}
+
+}
