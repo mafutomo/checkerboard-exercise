053c8f0c4dce65e2ddb60fffd354ee94d2174a97
Melissa Utomo <mafutomo@gmail.com>
2017-10-11 19:51:08 -0600

    updated gradient script with starting code

--- script.js
+++ script.js
@@ -3,44 +3,44 @@
 // Set each tile's float property to left
 // Each tile's paddingBottom is 11.1%// Your JS goes here
 
-// STARTING WITH RED
+// STARTING WITH DODGER BLUE
 
-for (let i = 0; i < 5; i++ ) {
+for (let i = 0; i < 5; i++) {
 
-for (let i = 0; i < 9; i++) {
+  for (let i = 0; i < 9; i++) {
 
-var div = document.createElement('div');
-var body = document.getElementsByTagName('body')[0];
+    var div = document.createElement('div');
+    var body = document.getElementsByTagName('body')[0];
 
-div.style.float = 'left';
-div.style.paddingBottom = '11.1%';
-div.style.width = '11.1%';
-body.prepend(div);
-if (i % 2 === 0) {
-  div.style.backgroundColor = 'black';
-} else {
-  div.style.backgroundColor = 'red';
-}
+    div.style.float = 'left';
+    div.style.paddingBottom = '11.1%';
+    div.style.width = '11.1%';
+    body.prepend(div);
 
-}
+    if (i % 2 === 0) {
+      div.style.backgroundColor = 'rgb(30, 144, 255)'; //dodgerblue
+    } else {
+      div.style.backgroundColor = 'rgb(106, 90, 205)'; //slate blue
+    }
 
-// STARTING WITH BLACK
-for (let i = 0; i < 9; i++) {
+  }
 
-var div = document.createElement('div');
-var body = document.getElementsByTagName('body')[0];
+  // STARTING WITH BLACK
+  for (let i = 0; i < 9; i++) {
 
-div.style.float = 'left';
-div.style.paddingBottom = '11.1%';
-// div.style.backgroundColor = 'red';
-div.style.width = '11.1%';
-body.prepend(div);
+    var div = document.createElement('div');
+    var body = document.getElementsByTagName('body')[0];
 
-if (i % 2 === 0) {
-  div.style.backgroundColor = 'red';
-} else {
-  div.style.backgroundColor = 'black';
-}
-}
+    div.style.float = 'left';
+    div.style.paddingBottom = '11.1%';
+    div.style.width = '11.1%';
+    body.prepend(div);
+
+    if (i % 2 === 0) {
+      div.style.backgroundColor = 'rgb(106, 90, 205)';
+    } else {
+      div.style.backgroundColor = 'rgb(30, 144, 255)';
+    }
+  }
 
 }
